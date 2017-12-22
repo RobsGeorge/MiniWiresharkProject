2bb5f778b32b3afc56749ac1df0a286f410a7fa0
RobsGeorge <33100615+RobsGeorge@users.noreply.github.com>
2017-12-22 19:41:46 +0200

    Add files via upload

--- MainPageController.java
+++ MainPageController.java
@@ -12,8 +12,12 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicBoolean;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -28,11 +32,14 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import static networksproject.vipersteam.MainPageController.randomAlphaNumeric;
 import org.jnetpcap.JBufferHandler;
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapDumper;
 import org.jnetpcap.PcapIf;
+import org.jnetpcap.packet.JHeader;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
 import org.jnetpcap.protocol.network.Ip4;
@@ -44,7 +51,78 @@ import org.jnetpcap.protocol.tcpip.Tcp;
  */
 public class MainPageController implements Initializable {
 
+ public class Item {
+        public Date date = new Date();
+        public SimpleStringProperty source = new SimpleStringProperty();
+        public SimpleStringProperty destination = new SimpleStringProperty();
+        public SimpleStringProperty protocol = new SimpleStringProperty();
+        public SimpleIntegerProperty length = new SimpleIntegerProperty();
+        public SimpleIntegerProperty headerLength = new SimpleIntegerProperty();
+        public SimpleStringProperty info = new SimpleStringProperty();
+        
+        public void setDate(Date d)
+        {
+            this.date = d;
+        }
+        public void setSource(SimpleStringProperty d)
+        {
+            this.source = d;
+        }
+        public void setDestination(SimpleStringProperty d)
+        {
+            this.destination = d;
+        }
+        public void setProtocol(SimpleStringProperty d)
+        {
+            this.protocol = d;
+        }
+        public void setLength(SimpleIntegerProperty d)
+        {
+            this.length = d;
+        }
+        public void setHeaderLength(SimpleIntegerProperty d)
+        {
+            this.headerLength = d;
+        }
+        public void setInfo(SimpleStringProperty d)
+        {
+            this.info = d;
+        }
+        
+        public Long getDate() {
+            return date.getTime();
+        }
 
+        public String getSource() {
+            return source.get();
+        }
+
+        public String getDestination() {
+            return destination.get();
+        }
+
+        public Integer getLength() {
+            return length.get();
+        }
+        
+        public Integer getHeaderLength() {
+            return headerLength.get();
+        }
+        
+        public String getProtocol()
+        {
+            return protocol.get();
+        }
+        
+        public String getInfo()
+        {
+            return info.get();
+        }
+    }
+
+    
+    
+    
 Stage stage; 
 Parent root;
 
@@ -61,11 +139,8 @@ public static String loadTextBoxOutput;
 public static String ofile;
 private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-private final TableView<PC> table = new TableView<>();
-private final ObservableList<PC> data =
-            FXCollections.observableArrayList(new PC(1,"192.168.1.1","255.255.255.255",1560,20,"HTTP","NOOO"));
-
-
+    
+    
 @FXML
 Button gotoWiresharkButton;
 @FXML
@@ -103,56 +178,62 @@ Button loadSelectedButton;
 @FXML
 Button goBackButton;
 @FXML
-TableView<PC> mainTableView;
-@FXML
-TableColumn column1;
-@FXML
-TableColumn column2;
-@FXML
-TableColumn column3;
-@FXML
-TableColumn column4;
-@FXML
-TableColumn column5;
-@FXML
-TableColumn column6;
-@FXML
-TableColumn column7;
+Button testTableButton;
 
+    // The table and columns
+    @FXML
+    TableView<Item> itemTbl;
 
-    public MainPageController() throws IOException { 
+    
+    @FXML
+    TableColumn itemDateCol;
+    @FXML
+    TableColumn itemSourceCol;
+    @FXML
+    TableColumn itemDestCol;
+    @FXML
+    TableColumn itemProtocolCol;
+    @FXML
+    TableColumn itemLengthCol;
+    @FXML
+    TableColumn itemHeaderLengthCol;
+    @FXML
+    TableColumn itemInfoCol;
+    
+            
+
+    // The table's data
+    ObservableList<Item> data;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Set up the table data
         MainPageController.snaplen = 64 * 1024;
         MainPageController.flags =  Pcap.MODE_PROMISCUOUS;
         MainPageController.timeout =  10 * 1000;
         ofile = "";
         
+       
     }
 
 
 public static String randomAlphaNumeric(int count) {
-
-StringBuilder builder = new StringBuilder();
-
-while (count-- != 0) {
-int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
-builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+    StringBuilder builder = new StringBuilder();
+    while (count-- != 0) {
+        int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+        builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+    }
+    return builder.toString();
 }
-
-return builder.toString();
-
-}
-    
-    
     public void gotoWiresharkHandleButton(ActionEvent event) throws IOException {     
       stage=(Stage) gotoWiresharkButton.getScene().getWindow();
       root = FXMLLoader.load(getClass().getResource("Page1.fxml"));
       Scene scene = new Scene(root);
       stage.setScene(scene);
-      stage.show(); 
+      stage.show();
     }
     
     public void gotoStartCapturing(ActionEvent event) throws IOException {
-           
       stage=(Stage) startCapturingButton.getScene().getWindow();
       root = FXMLLoader.load(getClass().getResource("Capturing.fxml"));
       Scene scene = new Scene(root);
@@ -186,7 +267,6 @@ return builder.toString();
         ArrayList<String> ListOfDescriptions;
         ListOfDescriptions = new ArrayList<>();
         
-        
         ListOfDevices = new ArrayList<>();
         
         for (PcapIf device : alldevs) {
@@ -196,11 +276,8 @@ return builder.toString();
                 ListOfDescriptions.add(description);
                 ListOfDevices.add(device);
             }
-            }
-            networkInterfacesComboBox.getItems().addAll(ListOfDescriptions);
-            
-            
-                        	
+        }
+        networkInterfacesComboBox.getItems().addAll(ListOfDescriptions);
     }
     
     
@@ -222,41 +299,49 @@ return builder.toString();
         startCapturingButton.setVisible(true);
     }
     
-    
-    
-    public void newCaptureHandler(ActionEvent event) throws IOException{
+    public void newCaptureHandler(ActionEvent event) throws IOException{           
         
+        itemDateCol.setCellValueFactory(new PropertyValueFactory<Item,Long>("date"));
+        itemSourceCol.setCellValueFactory(new PropertyValueFactory<Item,String>("source"));
+        itemDestCol.setCellValueFactory(new PropertyValueFactory<Item,String>("destination"));
+        itemProtocolCol.setCellValueFactory(new PropertyValueFactory<Item,String>("protocol"));
+        itemLengthCol.setCellValueFactory(new PropertyValueFactory<Item,Integer>("length"));
+        itemHeaderLengthCol.setCellValueFactory(new PropertyValueFactory<Item,Integer>("headerLength"));
+        itemInfoCol.setCellValueFactory(new PropertyValueFactory<Item,String>("info"));
+        data = FXCollections.observableArrayList();
+        itemTbl.setItems(data);
+        
+            lab1.setText("No. of Packages to Capture:");
+            numberOfPacketsTextBox.setVisible(true);
+            newCaptureButton.setText("Start");
+            lab1.setVisible(true);
+            String no = numberOfPacketsTextBox.getText().toString();
+            if(numberOfPacketsTextBox.getText().trim().isEmpty())
+            {
+                return;
+            }
+            loadButton.setVisible(false);
+            testLabel.setText("Capturing...");
+            numberOfPacketsTextBox.setVisible(false);
+            lab1.setVisible(false);
+            pcap =  Pcap.openLive(dev.getName(), snaplen, flags, timeout, errbuf);
                 
-                lab1.setText("No. of Packages to Capture:");
-                numberOfPacketsTextBox.setVisible(true);
-                newCaptureButton.setText("Start");
-                lab1.setVisible(true);
-                String no = numberOfPacketsTextBox.getText().toString();
-                if(numberOfPacketsTextBox.getText().trim().isEmpty())
-                {
-                    return;
-                }
-                loadButton.setVisible(false);
-                testLabel.setText("Capturing...");
-                numberOfPacketsTextBox.setVisible(false);
-                lab1.setVisible(false);
-		pcap =  Pcap.openLive(dev.getName(), snaplen, flags, timeout, errbuf);
-                
-                ofile = randomAlphaNumeric(4)+".cap";
-                
-                PcapDumper dumper = pcap.dumpOpen(ofile); // output file          
-                
-                PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() 
-                {   Tcp tcp = new Tcp();
-                    Ip4 ip = new Ip4();
-                    
+            ofile = randomAlphaNumeric(4)+".cap";
+               
+            PcapDumper dumper = pcap.dumpOpen(ofile); // output file          
+              
+            PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() 
+            {   Tcp tcp = new Tcp();
+                Ip4 ip = new Ip4();    
 		public void nextPacket(PcapPacket packet, String user) 
+                {
+                    byte[] sIP = new byte[4];
+                    byte[] dIP = new byte[4];
+                    
+                    if (packet.hasHeader(ip) == true) 
                     {
-                        byte[] sIP = new byte[4];
-			byte[] dIP = new byte[4];
-			              
-		if (packet.hasHeader(ip) == true) 
-                    {
+                        
+                    
                     sIP = packet.getHeader(ip).source();
                     dIP = packet.getHeader(ip).destination();
                     String sourceIP = org.jnetpcap.packet.format.FormatUtils.ip(sIP);
@@ -265,36 +350,70 @@ return builder.toString();
                       " dstIP=" + destinationIP + 
                       " caplen=" + packet.getCaptureHeader().caplen()+ 
                       " timeStamp=" +  new Date(packet.getCaptureHeader().timestampInMillis())+
-                      " original length=" + packet.getCaptureHeader().wirelen());              
-                    }
-                                            
+                      " original length=" + packet.getCaptureHeader().wirelen()
+                    + "header length= " + packet.getCaptureHeader().hdr_len() + "payload " + ip.getPayload().toString());
+                    
+                    Item item = new Item();
+                    item.setDate(new Date(packet.getCaptureHeader().timestampInMillis()));
+                    item.setSource(new SimpleStringProperty(sourceIP));
+                    item.setDestination(new SimpleStringProperty(destinationIP));
+                    item.setProtocol(new SimpleStringProperty("IPv4"));
+                    item.setLength(new SimpleIntegerProperty(packet.getTotalSize()));
+                    item.setHeaderLength(new SimpleIntegerProperty(packet.getCaptureHeader().hdr_len()));
+                    item.setInfo(new SimpleStringProperty(ip.getPayload().toString()));
+                    data.add(item);
+                    
+                    }                        
                     if (packet.hasHeader(tcp)) 
-                    {            
+                    {
+                        byte[] sTcp = new byte[4];
+                        byte[] dTcp = new byte[4];
+                        String sourceTcp = org.jnetpcap.packet.format.FormatUtils.ip(sIP);
+                        String destinationTcp = org.jnetpcap.packet.format.FormatUtils.ip(dIP);
+                    
+                    Item item = new Item();
+                    item.setDate(new Date(packet.getCaptureHeader().timestampInMillis()));
+                    item.setSource(new SimpleStringProperty(sourceTcp));
+                    item.setDestination(new SimpleStringProperty(destinationTcp));
+                    item.setProtocol(new SimpleStringProperty("TCP"));
+                    item.setLength(new SimpleIntegerProperty(tcp.getLength()));
+                    item.setHeaderLength(new SimpleIntegerProperty(tcp.getHeaderLength()));
+                    item.setInfo(new SimpleStringProperty(tcp.getPayload().toString()));
+                    data.add(item);
+                    
                     System.out.println("found packet with tcp payload=" + tcp.getPayload());
                     if(tcp.source() == 80)
                         {
-                            System.out.println("HTTP protocol");
-                        } 
-                    }	
+                            System.out.println("HTTP");
+                            
+                    item.setDate(new Date(packet.getCaptureHeader().timestampInMillis()));
+                    item.setSource(new SimpleStringProperty(sourceTcp));
+                    item.setDestination(new SimpleStringProperty(destinationTcp));
+                    item.setProtocol(new SimpleStringProperty("HTTP"));
+                    item.setLength(new SimpleIntegerProperty(tcp.getLength()));
+                    item.setHeaderLength(new SimpleIntegerProperty(tcp.getHeaderLength()));
+                    item.setInfo(new SimpleStringProperty(tcp.getPayload().toString()));
+                    data.add(item);
+                        }
+                    
                     }
-                };
-                
-                
-		 //capture first 100 packages
-                int numberOfPacketsToBeCaptured = Integer.parseInt(no);
-		pcap.loop((numberOfPacketsToBeCaptured/2)+numberOfPacketsToBeCaptured%2, jpacketHandler, "VipersTeam");
-                pcap.loop((numberOfPacketsToBeCaptured/2)+numberOfPacketsToBeCaptured%2,dumper);
-                dumper.close();
-                lab1.setVisible(true);
-                
-		pcap.close();
-                lab1.setText("");
-                testLabel.setText("Captured Successfully!!");
-                saveButton.setVisible(true);
-                discardButton.setVisible(true);
-                newCaptureButton.setText("New Capture");
-                newCaptureButton.setVisible(false);
-                       
+                    
+                }
+            };
+            //capture first 100 packages
+            int numberOfPacketsToBeCaptured = Integer.parseInt(no);
+            pcap.loop((numberOfPacketsToBeCaptured/2)+numberOfPacketsToBeCaptured%2, jpacketHandler, "VipersTeam");
+            pcap.loop((numberOfPacketsToBeCaptured/2)+numberOfPacketsToBeCaptured%2,dumper);
+            dumper.close();
+            lab1.setVisible(true);
+              
+            pcap.close();
+            lab1.setText("");
+            testLabel.setText("Captured Successfully!!");
+            saveButton.setVisible(true);
+            discardButton.setVisible(true);
+            newCaptureButton.setText("New Capture");
+            newCaptureButton.setVisible(false);
     }
     
     @FXML 
@@ -315,22 +434,18 @@ return builder.toString();
     }
     
     public void savePackets (ActionEvent event) throws IOException {
-        
         lab1.setText("Saved as "+ofile+" !");
         testLabel.setText("");
         discardButton.setVisible(false);
         newCaptureButton.setVisible(true);
         loadButton.setVisible(true);
         saveButton.setVisible(false);
-        numberOfPacketsTextBox.setText("");
-        
+        numberOfPacketsTextBox.setText("");    
     }
     
     
     
     public void loadFile (ActionEvent event) throws IOException {
-        
-        
         saveButton.setVisible(false);
         loadButton.setVisible(false);
         newCaptureButton.setVisible(false);
@@ -341,9 +456,6 @@ return builder.toString();
         lab1.setVisible(true);
         lab1.setText("Enter file name to load");
         loadSelectedButton.setVisible(true);
-        
-        
-      
     }
     
     public void LoadSelectedButtonHandler(ActionEvent event) throws IOException {
@@ -363,6 +475,7 @@ return builder.toString();
                 {   Tcp tcp = new Tcp();
                     Ip4 ip = new Ip4();
                     
+                @Override
 		public void nextPacket(PcapPacket packet, String user) 
                     {
                         byte[] sIP = new byte[4];
@@ -406,9 +519,10 @@ return builder.toString();
         }
     }
     
-   
-   
-    
+    public  void testTableButtonHandler (ActionEvent event) throws IOException {     
+        
+    }
+
     public void goBackButtonHandler (ActionEvent event) throws IOException {
       stage=(Stage) goBackButton.getScene().getWindow();
       root = FXMLLoader.load(getClass().getResource("Page1.fxml"));
@@ -417,9 +531,6 @@ return builder.toString();
       stage.show();
     }
     
-       @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-    } 
+  
     
 }
